b96dc2c6a0e1c9a0c206e19da51d58f4e84f17a5
cheliyasmileyqueen-afk <cheliyasmileyqueen@gmail.com>
2026-05-31 15:57:48 +0800

    Add files via upload
    Day 1 :First python program added with variables like strings,int,float and type ,arithmetic & relational operations ,print and type()

---
+++ first python program.py
@@ -0,0 +1,68 @@
+print("Hello World")
+print("Iam Cheliya")
+print("Iam from sankethika college")
+print("Iam a first year student from CSE","FISRT YEAR STUDENT")
+print(23)
+print(65+23)
+name ="cheliya"
+print(name)
+name ="charan"
+print(name)
+print("name")
+print("My name: ",name)
+name2=name
+print(name2)
+name ="Cheliya"
+Age = 18
+price=99.11
+
+#type of data
+
+print(type(name))
+print(type(Age))
+print(type(price))
+print('name')
+name= "cheliya"
+name1='cheliya'
+name2='''cheliya'''
+print(name)
+print(name2)
+print(name1)
+age =18
+old =False
+a=None
+print(type(age))
+print(type(old))
+print(type(a))
+
+#sum addition
+a =166
+b=789
+
+sum =a+b
+print(sum)
+
+#sum subraction
+sum=a-b
+print(sum)
+#print("Hello world")
+print("Hello world")
+print("Hello world")
+#arithmetic operators
+a=10
+b=17
+sum =a+b
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)  #for reminder
+print(a**b)  # a power b
+
+#relational operators
+a=50
+b=60
+print(a==b)  #output will be false
+print(a!=b)  #output will be true
+print(a>=b)  #it will be false
+print(a<=b)  #true
